c8cf7e54cc941436b7489f0d2de32e7761fa9093
vinayakhegde1 <73926849+vinayakhegde1@users.noreply.github.com>
2020-11-26 22:32:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#define Function
+#a collection of instructions
+#a collection of code
+def function1():
+    print("Python")
+    print("Programming")
+print("this is a function")
+function1()#Calling the function so it prints the code under the def function1
+# fucntion can also be a mapping
+def max_of_two(x,y):
+    if x>y:
+        return x      #return the output
+    return y          #return the output
+a= max_of_two(10,6)   #Here 10 is called the return value or output
+print(a)
+#Conver Miles into Killometer using Function
+def Convert_in_Km(miles):
+    return( 1.6*miles)
+print("The miles to Km is ")
+x=Convert_in_Km(20)
+print(x)
+
+
+
